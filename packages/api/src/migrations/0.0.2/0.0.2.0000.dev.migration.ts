b378ce6cde1e400d1f362712f9c4918457ef588b
Alan Strickland <alan.strickland@redtechnology.com>
2023-11-20 10:27:47 +0000

    Remove example model from migration.

--- packages/api/src/migrations/0.0.2/0.0.2.0000.dev.migration.ts
+++ packages/api/src/migrations/0.0.2/0.0.2.0000.dev.migration.ts
@@ -4,17 +4,6 @@ import { defineMigration, Context } from '@red-technology/tripla-api';
 export default defineMigration(async (context: Context): Promise<void> => {
   const app = { repositories: { models: context.api.repositories.models(context), documents: context.api.repositories.documents(context) } };
 
-  //Create a new model
-  const m: tm.Model = new tm.Model('my-model', 'My Model', 'Description of my model to be displayed in the admin', 'my-app/sub-section/00000', 'mdi-cog', {
-    properties: {
-      string: new tm.StringProperty('string', 'Example string property', { default: 'a', enum: ['a', 'b', 'c'] }),
-    },
-    required: [],
-    additionalProperties: false,
-  });
-
-  console.log('CUSTOM MIGRATION');
-
   app.repositories.documents.upsert('channel', {
     modelid: 'channel',
     id: 'tripla',
@@ -46,7 +35,4 @@ export default defineMigration(async (context: Context): Promise<void> => {
       default_category_state: true,
     },
   });
-
-  //Add the model
-  app.repositories.models.upsert(m);
 });
