5ddc1f2a74ce731f8602c7989e1b77df19b3ba17
Alan Strickland <alan.strickland@redtechnology.com>
2023-11-30 08:43:28 +0000

    Use the new patch method to configure the already installed channel instead of upsert

--- packages/api/src/migrations/0.0.2/0.0.2.0000.dev.migration.ts
+++ packages/api/src/migrations/0.0.2/0.0.2.0000.dev.migration.ts
@@ -4,35 +4,9 @@ import { defineMigration, Context } from '@red-technology/tripla-api';
 export default defineMigration(async (context: Context): Promise<void> => {
   const app = { repositories: { models: context.api.repositories.models(context), documents: context.api.repositories.documents(context) } };
 
-  app.repositories.documents.upsert('channel', {
+  app.repositories.documents.patch({
     modelid: 'channel',
     id: 'tripla',
-    name: 'Tripla',
     host: 'localhost:2010',
-    path: '/',
-    brand: {
-      modelid: 'brand',
-      id: 'tripla',
-    },
-    theme: {
-      modelid: 'theme',
-      id: 'tripla',
-    },
-    head: {
-      modelid: 'head',
-      id: 'tripla',
-    },
-    locale: {
-      modelid: 'locale',
-      id: 'en_gb',
-    },
-    enable_cookie_consent: true,
-    cookie_policy_configuration: {
-      cookie_policy: {
-        modelid: 'cookie-policy',
-        id: '332f8266-cda8-4cdc-a75b-381ad06ce729',
-      },
-      default_category_state: true,
-    },
   });
 });
